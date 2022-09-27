0134cbba94ca241e17047470048b4569a9146d96
ericabao <eric.abao@gmail.com>
2022-09-27 21:35:32 +0800

    converting social media icons to stripe

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu-mobile-wrap");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("hamburger-active");
-    navMenu.classList.toggle("nav-menu-mobile-wrap-active");
+hamburger.classList.toggle("hamburger-active");
+navMenu.classList.toggle("nav-menu-mobile-wrap-active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("hamburger-active");
-    navMenu.classList.remove("nav-menu-mobile-wrap-active");
+hamburger.classList.remove("hamburger-active");
+navMenu.classList.remove("nav-menu-mobile-wrap-active");
 }))
